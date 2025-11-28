4438b3b46e7345a3c4c0f2c13cec181e6818e954
Roman <romansola123@gmail.com>
2025-11-28 10:03:19 -0300

    Se continuo con el bootcamp

---
+++ apply-button.js
@@ -0,0 +1,12 @@
+
+const jobsListingSection = document.querySelector('.jobs-listings');
+
+jobsListingSection?.addEventListener('click', (event) => {
+    const element = event.target;
+
+    if (element.classList.contains('button-apply-job'))
+        element.textContent = "Aplicado";
+        element.classList.add('is-applied');
+        element.disabled = true;
+});
+
---
+++ main.js
@@ -0,0 +1,5 @@
+import './devjobs-avatar-element.js'
+
+import './apply-button.js'
+import './fetch-data.js'
+import './filters.js'
